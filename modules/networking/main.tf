13a0f9745ac1d2f3600b9004afaa8fa8a9b740d1
Noemie Thirot <lewisdylann7@gmail.com>
2026-03-05 07:32:41 -0500

    Docs: Updated architecture diagram to show RBAC and Availability Sets

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -34,3 +34,23 @@ resource "azurerm_subnet" "pe_snet" {
   
   private_endpoint_network_policies = "Enabled"
 }
+
+
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "${var.prefix}-db-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+
+  # Allow SQL traffic ONLY from the Web Subnet (Port 1433)
+  security_rule {
+    name                       = "AllowSQLFromWeb"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "10.0.1.0/24" 
+    destination_address_prefix = "*"
+  }
+}
